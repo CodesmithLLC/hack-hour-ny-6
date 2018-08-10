6f9d9034cb6e424dcd35e78c7250cd7e8019fc8d
Den Temple <developer@dentemple.com>
2018-08-09 23:26:21 -0400

    Improve one-liner

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -18,15 +18,11 @@
 //                     'fizzbuzz',
 //                     16 ]
 
-/* Solution 2 */
 const fizzbuzz = num =>
   num >= 0 &&
-  [num]
-    .map(num => [...Array(num + 1).keys()])
-    .reduce((acc, arrayOfArray) => acc.concat(arrayOfArray), [])
-    .map((num, i) => (num % 3 === 0 && num % 5 === 0 ? "fizzbuzz" : i))
-    .map(val => (val % 3 === 0 && typeof val === "number" ? "fizz" : val))
-    .map(val => (val % 5 === 0 && typeof val === "number" ? "buzz" : val))
+  Array(num)
+    .fill("")
+    .map((_, i) => i)
+    .map(num => [["fizz"][num % 3], ["buzz"][num % 5]].join("") || num)
 
-// console.log(fizzbuzz(30))
 module.exports = fizzbuzz
