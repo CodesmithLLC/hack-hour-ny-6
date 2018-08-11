2fb752cd02ddc319ff4376c504ee3e5748a33155
Den Temple <developer@dentemple.com>
2018-08-11 09:49:37 -0400

    Complete challenge

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -2,16 +2,41 @@
  * Given an array of numbers, determine if the mode and mean of the array are equivalent
  *
  * Caveats:
- * 	- Math.floor the mean
- * 	- If there are multiple modes, use the max of the modes
+ * 	- Math.floor the mean // average
+ * 	- If there are multiple modes, use the max of the modes // value that occurs most often
  *
  * Return true or false
  *
  */
 
-
 function modemean(array) {
+  return getMean(array) === getMode(array)
 
+  // -----
+
+  function getMean(array) {
+    return Math.floor(array.reduce((num, element) => (num += element), 0) / array.length)
+  }
+
+  function getMode(array) {
+    const memo = array.reduce((acc, element) => {
+      return acc[element] === undefined
+        ? // if not found, add to object
+          { ...acc, [element]: 1 }
+        : // if found, increment the counter
+          { ...acc, [element]: (acc[element] += 1) }
+    }, {})
+    return (
+      // get list of properties
+      Object.keys(memo)
+        // convert each to a more workable object
+        .map(prop => ({ element: memo[prop], value: memo[prop] }))
+        // sort by value, then return the first element (which would be the max)
+        .sort((a, b) => b.value - a.value)[0].element
+    )
+  }
 }
 
-module.exports = modemean;
+console.log(modemean([1, 2, 2, 2, 3, 3, 4]))
+
+module.exports = modemean
