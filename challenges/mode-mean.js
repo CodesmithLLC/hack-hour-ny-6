2c27a12f4835dfda7c4c1b147aff8b1aca035975
Mike <mmarceca@aardvarkbrigade.com>
2018-08-11 10:07:09 -0400

    finished mode mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,31 @@
 
 
 function modemean(array) {
-
+    array = array.sort((a, b) => a - b);
+    let mode = array[0];
+    let prev = array[0];
+    counter = 0;
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] !== prev) {
+            counter = 0;
+            prev = array[i]
+        }
+        counter++;
+        if(counter > mode){
+            mode = array[i];
+        }
+    }
+    
+    let add =0;
+    for(let i = 0; i < array.length; i++) {
+        add += array[i];
+    }
+    add = add / array.length;
+    if(mode === add) {
+        return true;
+    } else {
+        return false;
+    }
 }
-
-module.exports = modemean;
+console.log(modemean([0,1,3,2,4]))
+// module.exports = modemean;
