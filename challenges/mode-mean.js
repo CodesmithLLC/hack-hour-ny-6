0af5fda0146ee4dbb61da4eefb5b34d8c51f4e07
Den Temple <developer@dentemple.com>
2018-08-11 10:02:07 -0400

    Solve certain edge cases prior to formal submission

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -15,28 +15,29 @@ function modemean(array) {
   // -----
 
   function getMean(array) {
-    return Math.floor(array.reduce((num, element) => (num += element), 0) / array.length)
+    return Math.floor(
+      array.reduce((num, element) => (num += element), 0) / array.filter(num => num !== 0).length
+    )
   }
 
   function getMode(array) {
     const memo = array.reduce((acc, element) => {
-      return acc[element] === undefined
+      return acc[element + ""] === undefined
         ? // if not found, add to object
-          { ...acc, [element]: 1 }
+          { ...acc, [element + ""]: 1 }
         : // if found, increment the counter
-          { ...acc, [element]: (acc[element] += 1) }
+          { ...acc, [element + ""]: (acc[element] += 1) }
     }, {})
+
     return (
       // get list of properties
       Object.keys(memo)
         // convert each to a more workable object
-        .map(prop => ({ element: memo[prop], value: memo[prop] }))
+        .map(prop => ({ element: prop, value: memo[prop] }))
         // sort by value, then return the first element (which would be the max)
         .sort((a, b) => b.value - a.value)[0].element
     )
   }
 }
-
-console.log(modemean([1, 2, 2, 2, 3, 3, 4]))
 
 module.exports = modemean
