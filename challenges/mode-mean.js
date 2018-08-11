cfb89f7defd5d3e08a7e090a2b894b75fd6f5231
Kwame Amosah <kwameamosah@Kwames-MacBook-Pro.local>
2018-08-11 09:52:12 -0400

    modemean complete

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,21 @@
 
 
 function modemean(array) {
-
+    let count = {};
+    for(let i = 0; i < array.length; i++) {
+      count[array[i]] ? count[array[i]]++ : count[array[i]] = 1;
+    }
+    let mode = array[0];
+    let maxFreq = 0;
+    for(let num in count) {
+      if(count[num] >= maxFreq) {
+        mode = Math.max(num, most);
+        maxFreq = count[num];
+      }
+    }
+    return Math.floor(array.reduce(function (a, b) {
+      return a + b;
+    }) / array.length) === moded;
 }
 
 module.exports = modemean;
