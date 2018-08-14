f1056e094f7c658a29411a606e7ef7861dacf275
Den Temple <developer@dentemple.com>
2018-08-14 09:37:57 -0400

    Hack hour for kth-last-node

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -1,28 +1,49 @@
 /**
  * Write a function that takes two parameters, an integer and the head of a
  * singly linked list, and returns the VALUE on the kth to last node in the list.
- *
- * const a = new Node('A');
- * const b = new Node('B');
- * const c = new Node('C');
- * const d = new Node('D');
- * const e = new Node('E');
- *
- * a.next = b;
- * b.next = c;
- * c.next = d;
- * d.next = e;
- *
- * kthToLastNode(2, a); -> returns 'D' (the value on the second to last node)
  */
+// const a = new Node("A")
+// const b = new Node("B")
+// const c = new Node("C")
+// const d = new Node("D")
+// const e = new Node("E")
+
+// a.next = b
+// b.next = c
+// c.next = d
+// d.next = e
+
+// console.log(kthToLastNode(3, a)) // -> returns 'D' (the value on the second to last node)
 
 function Node(val) {
-  this.value = val;
-  this.next = null;
+  this.value = val
+  this.next = null
 }
 
 function kthToLastNode(k, head) {
+  if (k >= 0) {
+    let counter = 0
+    let memo = {}
+    let node = head
 
+    while (node !== null) {
+      if (counter >= k) {
+        memo = { ...memo, [counter]: node.value }
+      }
+      node = node.next
+      counter++
+    }
+
+    const keys = Object.keys(memo)
+
+    if (keys.length > 1) {
+      return memo[keys[keys.length - 2]]
+    }
+
+    return null
+  }
+
+  return null
 }
 
-module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+module.exports = { Node: Node, kthToLastNode: kthToLastNode }
