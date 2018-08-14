3acfee66f185bc351d20bc0cfc236bb7d4e5ec4d
Mike <mmarceca@aardvarkbrigade.com>
2018-08-14 09:51:36 -0400

    finished kth number of linked list reversed

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -21,8 +21,49 @@ function Node(val) {
   this.next = null;
 }
 
-function kthToLastNode(k, head) {
 
+function kthToLastNode(k, head) {
+  let prev = head;
+  let curr = prev.next;
+  let counter = 0;
+  let count = {};
+  while(prev.next !== null) {
+    count[counter++] = prev.value;
+    prev = curr;
+    curr = prev.next;
+    if(prev.next === null) {
+      count[counter] = prev.value;
+    }
+  }
+  let reverseCount = Object.keys(count).length;
+  let indexWanted = reverseCount - k;
+  if(reverseCount < k) {
+    return 'invalid index request -- index is longer then items in list'
+  }
+  // console.log(reverseCount)
+  for(let prop in count) {
+    if(prop == indexWanted) {
+      return count[prop];
+    }
+  }
 }
 
-module.exports = {Node: Node, kthToLastNode: kthToLastNode};
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+const f = new Node('F');
+const g = new Node('G');
+
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+e.next = f;
+f.next = g;
+
+console.log(kthToLastNode(2, a));
+
+// module.exports = {Node: Node, kthToLastNode: kthToLastNode};
