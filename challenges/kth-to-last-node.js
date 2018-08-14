6a135a7ec273a79aead95cebfe168b660236ccb7
AaronAddison <abartaddison12@gmail.com>
2018-08-14 09:57:24 -0400

    finished kthToLastNode

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -2,13 +2,13 @@
  * Write a function that takes two parameters, an integer and the head of a
  * singly linked list, and returns the VALUE on the kth to last node in the list.
  *
- * const a = new Node('A');
- * const b = new Node('B');
- * const c = new Node('C');
- * const d = new Node('D');
- * const e = new Node('E');
+ * const a = new Node('A'); 1
+ * const b = new Node('B'); 2
+ * const c = new Node('C'); 3
+ * const d = new Node('D'); 4
+ * const e = new Node('E'); 5
  *
- * a.next = b;
+ * a.next = b; 
  * b.next = c;
  * c.next = d;
  * d.next = e;
@@ -22,7 +22,42 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
+  if( !head){
+    return "Not valid list!";
+  }
 
+  //Initialize items in list at 1 if head is valid
+  let itemsInList = 1;
+  let currNode = head;
+
+  if ( head.next === null){
+    return "Only one node in list!"
+  }
+
+  while( currNode.next !== null){
+    itemsInList++;
+    currNode = currNode.next;
+  }
+  
+  currNode = head;
+  for( let i = 1; i < itemsInList-(k-1); i++){
+    currNode = currNode.next
+  }
+
+  return currNode.value
 }
+
+// const a = new Node('A'); 
+// const b = new Node('B'); 
+// const c = new Node('C'); 
+// const d = new Node('D'); 
+// const e = new Node('E'); 
+
+// a.next = b; 
+// b.next = c;
+// c.next = d;
+// d.next = e;
+
+// console.log(kthToLastNode(3,a));
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
