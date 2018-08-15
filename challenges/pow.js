d9910776d77c0851dc89cb93bbc04b08a455d5bb
Den Temple <developer@dentemple.com>
2018-08-15 09:55:20 -0400

    Submit solution for pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,24 @@
  * Use recursion!
  */
 
-function pow(base, power) {
-
+function pow(base, power, acc = 1) {
+  if (power === 0) {
+    return 1
+  }
+  if (power === 1) {
+    return acc * base
+  }
+  if (power === -1) {
+    return acc / base
+  }
+  return pow(base, power > 0 ? power - 1 : power + 1, power > 0 ? acc * base : acc / base)
 }
 
-module.exports = pow;
+/* TDD */
+// console.log(pow(2, 2), "should be 4")
+// console.log(pow(2, 3), "should be 6")
+// console.log(pow(3, 3), "should be 27")
+// console.log(pow(8, -1), "should be 0.125")
+// console.log(pow(4, -3), "should be 0.015625")
+
+module.exports = pow
