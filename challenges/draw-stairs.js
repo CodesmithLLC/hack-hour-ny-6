2e69fde41c18e70bb071af521015ae3478550c51
Mike <mmarceca@aardvarkbrigade.com>
2018-08-17 19:29:10 -0400

    removed extra spacing

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -1,9 +1,9 @@
 /* Write a function that console logs a staircase of any given height where 1 <= N <= 100.
- The staircase must climb up from left to right. The last line should only consist of asterisks,
- without any leading/trailing spaces.
- 
- For example:     
- drawStairs(6) ->          
+The staircase must climb up from left to right. The last line should only consist of asterisks,
+without any leading/trailing spaces.
+
+For example:     
+drawStairs(6) ->          
      *
     **
    ***
