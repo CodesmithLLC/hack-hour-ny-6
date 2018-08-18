c074adc3490d4f9094f83cc5c6c2946f570c9791
Joel Pratt <pratt.joel@gmail.com>
2018-08-18 10:05:40 -0400

    string-rotation.

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -16,7 +16,41 @@ function isSubstring(s1, s2) {
 }
 
 function stringRotation(s1, s2) {
+  const arr1 = s1.split("");
+  const arr2 = s2.split("");
 
+  if (arr1.length !== arr2.length){
+    return false
+  }
+  console.log({arr1});
+  console.log({arr2});
+  const part1 = [];
+  const part2 = [];
+  let foundFirst = false
+
+  for (let i=0; i < arr1.length; i++){
+    if (foundFirst){
+      part1.push(arr2[i]);
+    } else if (arr2[i] === arr1[0]){
+        part1.push(arr2[i]);
+        foundFirst = true
+      } else {
+        part2.push(arr2[i]);
+      }
+    }
+    const word2 = [...part1, ...part2];
+    if (s1 === word2.join("")){
+      return true
+    }
+    return false
+    console.log({part1});
+    console.log({part2});
+    console.log({word2});
 }
+
+console.log(stringRotation("hello", "hello"))
+console.log(stringRotation("hello", "llohe"))
+console.log(stringRotation("hello", "he"))
+console.log(stringRotation("hello", "ollhe"))
 
 module.exports = {isSubstring: isSubstring, stringRotation: stringRotation};
