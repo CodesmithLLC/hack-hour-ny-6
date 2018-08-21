fbdbb92740eaf41dad8702f26a9a6ad514d0c9c8
Joel Pratt <pratt.joel@gmail.com>
2018-08-21 10:01:15 -0400

    balanced-parens submission.

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -26,6 +26,55 @@
 
 function balancedParens(input){
 
+  const inputArr = input.split("")
+  //console.log(inputArr);
+  // const openBracket = ['(', '[', '{'];
+  // const closedBracket = [')', ']', '}'];
+  const allBrackets = {
+    parens: { open: '(', closed: ')'},
+    bracket: { open: '[', closed: ']'},
+    curly: { open: '{', closed: '}'},
+  }
+
+  let isClosed = false;
+  let openBracket = ''
+  let closingBracket = ''
+  let countToNext = 0;
+  let allClosed = []
+//  let isClosed = true;
+
+  for (let i = 0; i < inputArr.length; i++){
+    countToNext ++
+    for (let bracketType in allBrackets){
+      if (inputArr[i] === allBrackets[bracketType].open){
+        // isOpen = true;
+        isClosed = false;
+      //  openBracket = allBrackets[bracketType].open;
+        closingBracket = allBrackets[bracketType].closed;
+        countToNext = 0;
+       console.log({openBracket: allBrackets[bracketType].open});
+      }
+    }
+    if (inputArr[i] === closingBracket && countToNext <= 1){
+      //currentBracket = inputArr[i]
+      console.log({closingBracket});
+      // return true
+      // isOpen = true;
+      isClosed = true;
+    }
+  }
+  return isClosed;
 }
+
+// console.log(balancedParens('('));
+// console.log(balancedParens('()'));
+// console.log(balancedParens(')('));
+// console.log(balancedParens('(())'));
+//
+// console.log(balancedParens('[](){}'));
+// console.log(balancedParens('[({})]'));
+// console.log(balancedParens('[(]{)}'));
+console.log(balancedParens('var wow  = { yo: thisIsAwesome() }'));
+console.log(balancedParens('var hubble = function() { telescopes.awesome();'));
 
 module.exports = balancedParens;
