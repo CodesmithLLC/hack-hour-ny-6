cd8ce915f403a6270b90b6e3f780c5278c5ca99e
AaronAddison <abartaddison12@gmail.com>
2018-08-22 10:01:05 -0400

    tried it :(

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,51 @@
  */
 
 function romanNumeral(n) {
+  let obj = {
+    1: 'I',
+    4: 'IV',
+    5: 'V',
+    9: 'IX',
+    10: 'X',
+    40: 'XL',
+    50: 'L',
+    90: 'XC',
+    100: 'C',
+    400: 'CD',
+    500: 'D',
+    900: 'CM',
+    1000: 'M'
+  }
 
+  let result = '';
+  n.toString().split('').forEach( (int, index) => {
+    let number = parseInt(int);
+    debugger;
+    if( obj.hasOwnProperty(number)) {result = result + obj[number];}
+    else {
+      switch( index ){
+        case 1:
+          result = result + obj[number].repeat(number) || int.repeat(int[0]);
+          break;
+        case 2:
+          result = result + obj[number].repeat(int[0]) || int.repeat(int[0]);
+          break;
+        case 3:
+          result = result + obj[number].repeat(int[0]) || int.repeat(int[0]);
+          break;
+        case 4:
+          result =  result + obj[number].repeat(int[0]) || int.repeat(int[0]);
+      }
+    }
+  })
+  debugger;
+  return result;
 }
+
+
+console.log('romanNumeral(4): ', romanNumeral(4));
+console.log('romanNumeral(55): ', romanNumeral(55));
+console.log('romanNumeral(102): ', romanNumeral(102));
+console.log('romanNumeral(200): ', romanNumeral(200));
 
 module.exports = romanNumeral;
--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -1,4 +1,4 @@
-/* Given two strings, write a function to check if the second one is a rotation of the first,
+ /* Given two strings, write a function to check if the second one is a rotation of the first,
  * using only one call to isSubstring. The function isSubstring takes in two strings and returns
  * true if the second string is found in the first, false otherwise.
  *
