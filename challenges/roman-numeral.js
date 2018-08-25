0df55cd17409dce4633599e3fcced8c925503a93
Joel Pratt <pratt.joel@gmail.com>
2018-08-25 09:58:37 -0400

    binary to decimal submission.

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,22 @@
  */
 
 function romanNumeral(n) {
-  const romanNumerals = { 1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M' };
+  const romanNumerals = {
+    1: 'I',
+    4: 'IV',
+    5: 'V',
+    9: 'IX',
+    10: 'X',
+    40: 'XV',
+    50: 'L',
+    90: 'XC',
+    100: 'C',
+    400: 'CD',
+    500: 'D',
+    900: 'CM',
+    1000: 'M'
+  };
+  
   let romNum = []
   let prevNum = 1
   for (num in romanNumerals){
