1be6808a753508bc60dde854ceed73f95bb6a6ec
AaronAddison <abartaddison12@gmail.com>
2018-08-29 09:48:47 -0400

    new updates for negative numbers

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,19 @@
  */
 
 function highestProduct(array) {
-  return array.sort((a,b) => a > b).slice(-3).reduce( prev, next => prev * next);
+  let sortArray = array.sort((a,b) => a > b);
+  let lastThree = sortArray.slice(-3).reduce( (prev, next) => prev * next);
+  let negatives = sortArray.filter( num => num < 0);
+
+  if (negatives.length === 2){
+    negatives = sortArray.slice(0,2)
+    negatives.push(sortArray[sortArray.length - 1])
+    negatives = negatives.reduce( (prev, next) => prev * next);
+  } 
+  
+  return Math.max(lastThree, negatives);
 }
 
+console.log(highestProduct([-9,-2,1, 3,4,5]))
 
 module.exports = highestProduct;
