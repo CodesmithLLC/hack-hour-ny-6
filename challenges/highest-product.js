3018a10e772ab4788ac72fbb56dcb896f75e9f64
Mike <mmarceca@aardvarkbrigade.com>
2018-08-29 09:23:24 -0400

    removed console

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -6,7 +6,6 @@ function highestProduct(array) {
   if(array.length < 3) return 'Need more then 3 items'
   array = array.sort((a,b) => {return a-b});
   array.reverse();
-  console.log(array)
   let sum = array[0] + array[1] + array[2]
   return sum;
 }
