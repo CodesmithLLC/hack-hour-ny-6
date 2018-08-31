678d8135a0f30e9d09a33c804b99094b52e3ff1a
Mike <mmarceca@aardvarkbrigade.com>
2018-08-31 10:33:24 -0400

    wip

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,45 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  let count = Object.keys(l1).length;
+  l3 = {};
+  let holder1 = {};
+  let holder2 = {};
+  while(l1.next !== undefined && l2.next !== undefined){
+    if(count % 2 === 0) {
+      l3[value] = l1.value;  
+      l3[next] = l2.next;
+      count--;
+    }
+    if(count % 2 === 1) {
+      l3[value] = l2.value;  
+      l3[next] =  l1.next;
+      count--;
+    }
+    l1.next();
+    l2.next()
+  }
 };
 
+let a = {
+  val = 4,
+  next = {
+    val = 5,
+    next = {
+      val = 6,
+      next = null
+    }
+  }
+};
+
+let b = {
+  val = 1,
+  next = {
+    val = 2,
+    next = {
+      val = 3,
+      next = null
+    }
+  }
+}
 module.exports = {Node: Node, zip: zip};
