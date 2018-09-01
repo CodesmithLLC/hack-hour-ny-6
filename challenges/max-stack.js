59c0e5798a59808d43bb2d83c6f335dbac8099f9
Den Temple <developer@dentemple.com>
2018-09-01 09:39:14 -0400

    max-stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,39 @@
  */
 
 function Stack() {
-  // body...
+  this.storage = {}
+  this.length = 0
+  this.highest = -Infinity
 }
 
-module.exports = Stack;
+Stack.prototype.push = function(value) {
+  this.storage[this.length] = value
+  this.highest = value > this.highest ? value : this.highest
+  this.length += 1
+  return this.length
+}
+
+Stack.prototype.pop = function() {
+  if (this.length > 0) {
+    const popped = this.storage[this.length - 1]
+    delete this.storage[this.length - 1]
+    this.length -= 1
+    return popped
+  }
+  return null
+}
+
+Stack.prototype.getMax = function() {
+  return this.highest
+}
+
+let stack = new Stack()
+stack.push(0)
+stack.push(1)
+stack.push(20)
+stack.push(3)
+console.log({ push: stack.push(4), stack })
+console.log({ pop: stack.pop(), stack })
+console.log({ getMax: stack.getMax() })
+
+module.exports = Stack
