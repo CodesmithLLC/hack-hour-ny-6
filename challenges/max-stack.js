5626c29e2a06d56febe8618a39b7e997daa25de3
Joel Pratt <pratt.joel@gmail.com>
2018-09-01 10:16:05 -0400

    max-stack submission

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -5,9 +5,70 @@
  * getMax should return the largest value currently in the stack.
  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
  */
+function Node(val){
+  this.value = val;
+  this.next = null;
+}
 
 function Stack() {
   // body...
+  this.storage = {};
+  this.index = 0;
+  this.max = 0;
+  //this.maxList = new Node(this.max);
+  // this.currentMax = null;
+
+  // console.log(this.index);
 }
+
+Stack.prototype.push = function(value) {
+  // console.log({this});
+  // console.log(this.index);
+  // if (value > this.maxList.value){
+  //   this.maxList.next = new Node(value);
+  // }
+  if (value > this.max){
+    this.max = value;
+  }
+
+  this.storage[this.index] = value;
+  this.index++
+  return this.index;
+}
+
+Stack.prototype.pop = function() {
+  const deletedItem = this.storage[this.index - 1];
+
+  // console.log({deletedItem});
+  delete this.storage[this.index - 1]
+  this.index--;
+  if (deletedItem === this.max) {
+    this.max = 0;
+    // console.log(this.max);
+    for (let val in this.storage){
+      if (this.storage[val] > this.max){
+        this.max = this.storage[val];
+      }
+    }
+  }
+  // console.log(this.max);
+  // console.log(this.storage);
+  return deletedItem;
+}
+
+Stack.prototype.getMax = function() {
+  return this.max
+}
+
+const stack = new Stack();
+
+console.log(stack.push(1));
+console.log(stack.push(8))
+console.log(stack.push(5))
+console.log(stack.push(2))
+console.log(stack.push(9))
+console.log(stack.pop())
+// console.log(stack.push(2))
+console.log(stack.getMax());
 
 module.exports = Stack;
--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,97 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
-};
+  let currentNode;
+  let l1Node = l1;
+  let l2Node = l2;
+
+  // console.log({l1Node});
+  // console.log({l2Node});
+  // let nextNode
+  let nodeCount = 0;
+
+  while (l1Node.next !== null && l2Node.next !== null){
+    // console.log({nodeCount});
+    // console.log({currentNode});
+    if (nodeCount === 0){
+      currentNode = l1Node;
+      // console.log({currentNode});
+
+    } else {
+      if (currentNode.value === l1Node.value) {
+          currentNode.next = l2Node;
+          l2Node = l2Node.next;
+          // break;
+      } else {
+          currentNode.next = l1Node;
+          l1Node = l1Node.next;
+          // break;
+        }
+    }
+      nodeCount++
+    }
+    // currentNode.next = currentL1Node;
+  return currentNode;
+  }
+  // let nodeCount = 0;
+  // while (currentNode.next !== null) {
+  //   console.log({currentNode});
+  //   console.log({nodeCount});
+  //   currentNode = currentNode.next;
+  //   nodeCount++
+  // }
+
+
+
+let node = new Node(0);
+let node1Curr;
+for (let i=0; i < 5; i++){
+  //console.log({node});
+  if (i===0){
+    node.next = new Node(i+1);
+    node1Curr = node.next;
+  } else {
+    node1Curr.next = new Node(i+1);
+    node1Curr = node1Curr.next
+  }
+}
+
+// while (node.next !== null) {
+//   console.log(node);
+//   node = node.next;
+// }
+
+let node2 = new Node(5);
+let node2Curr;
+//let currentNode = node;
+for (let i=0; i < 5; i++){
+  if (i===0){
+    node2.next = new Node(i+1);
+    node2Curr = node2.next;
+  } else {
+    node2Curr.next = new Node(i+1);
+    node2Curr = node2Curr.next
+  }
+  // node2.next = new Node(i+5);
+  // node2 = node2.next
+}
+
+// while (node2.next !== null) {
+//   console.log(node2);
+//   node2 = node2.next;
+// }
+
+// console.log({node});
+// console.log({node2});
+console.log({node1Curr});
+
+let outputNode = zip(node, node2);
+
+while (outputNode.next !== null) {
+  console.log({outputNode});
+  outputNode = outputNode.next;
+}
+
+console.log('output:', zip(node, node2));
 
 module.exports = {Node: Node, zip: zip};
