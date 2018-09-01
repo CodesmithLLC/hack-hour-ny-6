4d49aa6871b9ed98f6d96f2a4b26b329b44de5fa
christinechoi <smi77y@gmail.com>
2018-09-01 10:13:55 -0400

    Submit for max stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -3,11 +3,32 @@
  * push should return the new length of the stack.
  * pop should return the element that was just removed.
  * getMax should return the largest value currently in the stack.
- * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
+ * BONUS: The getMax method should retrieve the maximum value from 
+ * // the stack in O(1) time.
  */
 
 function Stack() {
-  // body...
+  this.count = 0;
+  this.max = null
+  this.storage = {};
 }
 
-module.exports = Stack;
+Stack.prototype.push = function(val) {
+  if (val > this.max) {this.max = val}
+  this.storage[this.count] = val
+  this.count++
+  return this.count 
+}
+
+Stack.prototype.pop = function() {
+  if (this.count === 0) return
+  --this.count
+  return this.storage[this.count];
+}
+
+Stack.prototype.getMax = function(value) {
+  return this.max
+}
+
+
+// module.exports = Stack;
