39d69e4aaaec0b4f5676d906019695669e734e08
AaronAddison <abartaddison12@gmail.com>
2018-09-01 12:02:46 -0400

    finished zip linked-list

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,52 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  let head = JSON.parse(JSON.stringify(l1));
+  let tmp = l1.next;
+  let curr = head;
+
+  while( tmp !== null || l2 !== null){
+      let node = l2;
+      // assign curr.next the current node on the right;
+      curr.next = node;
+      // reassign node on the right to its next value
+      l2 = l2.next;
+      //reassign curr to its next value
+      curr = curr.next;
+      // assign the tmp value to curr.next
+      curr.next = tmp;
+      // re-assign curr to it next value
+      curr = curr.next;
+      // re-assign tmp to its next value
+      if( tmp !== null) {
+        tmp = tmp.next
+      } else {
+        tmp = null;
+      }
+  }
+  return head;
 };
+
+let node1 = 
+{value: 3,
+ next : {
+   value: 2,
+   next: {
+     value: 1,
+     next: null
+   }
+ }}
+
+ let node2 = 
+{value: 10,
+ next : {
+   value: 9,
+   next: {
+     value: 8,
+     next: null
+   }
+ }}
+
+console.log(zip( node1, node2))
 
 module.exports = {Node: Node, zip: zip};
