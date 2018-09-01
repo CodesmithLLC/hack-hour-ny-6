2215d377a682090fc85fd7e091c49c4d4e68fb9e
Ryan Vazquez <ryanvazquez@Ryans-MacBook-Pro.local>
2018-09-01 13:18:47 -0400

    bugFixes & bonus

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,53 @@
  */
 
 function Stack() {
-  // body...
+  this.storage = {
+    head: null,
+    tail: null,
+  };
+  this.length = 0;
+  this.max = null;
+  this.createNode = (value, max) => {
+    return {
+      value: null || value,
+      prevMax: null || max,
+      prev: null,
+      next: null
+    }
+  };
+  this.push = (value) => {
+    const node = this.createNode(value, this.max);
+    if (!this.length) {
+      node.prev = this.storage;
+      this.storage.head = node;
+      this.storage.tail = node;
+      this.max = value;
+    } else {
+      node.prev = this.storage.tail;
+      this.storage.tail.next = node;
+      this.storage.tail = node;
+    }
+    if (value > this.max) {
+      this.max = value;
+    }
+    this.length += 1;
+    return this.length;
+  }
+  this.pop = () => {
+    if (!this.length) return undefined;
+    const { value } = this.storage.tail
+    this.storage.tail = this.storage.tail.prev;
+    this.storage.tail.next = null;
+    if (this.storage.tail.value > this.storage.tail.prevMax) {
+      this.max = this.storage.tail.value;
+    } else {
+      this.max = this.storage.tail.prevMax;
+    }
+    this.length -= 1;
+  };
+  this.getMax = () => {
+    return this.max;
+  }
 }
 
 module.exports = Stack;
