b46663441c3360fa10341b8e6838abafecd73166
Den Temple <developer@dentemple.com>
2018-09-08 09:57:05 -0400

    Solution for perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,37 @@
  */
 
 function permPalin(str) {
-	
+  if (!str || typeof str !== 'string') return false
+
+  const counts = [...str].reduce(
+    (acc, letter) => ({
+      ...acc,
+      [letter]: acc[letter] === undefined ? 1 : +acc[letter] + 1
+    }),
+    {}
+  )
+  const frequences = Object.values(counts)
+
+  if (str.length % 2 === 0) {
+    return frequences.reduce(
+      (bool, num) => (!bool ? false : num % 2 === 0),
+      false
+    )
+  } else {
+    let odds = 0
+    for (let i = 0; i < frequences.length; i++) {
+      if (frequences[i] % 2 !== 0) {
+        odds++
+      }
+      if (odds > 1) return false
+    }
+    return true
+  }
 }
 
-module.exports = permPalin;
+// console.log(permPalin('abab'), '=> true')
+// console.log(permPalin('cbaba'), '=> true')
+// console.log(permPalin('cbac'), '=> false')
+// console.log(permPalin('a'), '=> true')
+
+module.exports = permPalin
