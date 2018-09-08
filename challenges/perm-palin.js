1e77d3a86f497a6cd7c7d6c9ebf1543a3ba53205
christinechoi <smi77y@gmail.com>
2018-09-08 09:53:11 -0400

    Submit perm-palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,23 @@
  */
 
 function permPalin(str) {
-	
+  if (str.length === 1) return true 
+  let arr = str.split('')
+
+  let key = arr.reduce((acc, cur) => {
+    if (acc[cur]) acc[cur]++
+    else acc[cur] = 1
+    return acc 
+  }, {})
+
+  let valArr = Object.values(key)
+  let count = 0;
+  valArr.reduce((arr, cur) => { // find how many chars occur an odd number of times 
+    if (cur % 2 !== 0) count++ 
+    return count 
+  }, count)
+
+  return count === 1 || count === 0
 }
 
 module.exports = permPalin;
