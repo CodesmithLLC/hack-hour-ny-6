7b2f909498e69301bbb198eb977be2f2311b0540
Mike <mmarceca@aardvarkbrigade.com>
2018-09-08 09:53:37 -0400

    failed num to words completed perm palin

--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -13,7 +13,76 @@
  */
 
 function numToWords(num) {
-
+  let lowNumbers = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen'
+  ]
+  let tens = [
+    'twenty',
+    'thirty',
+    'fourty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety'
+  ]
+  let biggerNumbers = [
+    'hundred',
+    'thousand',
+    'million'
+  ]
+  if(num >= 0 && num <= 19) {
+    return lowNumbers[num];
+  }
+  let toString = '';
+  var arrays = [], size = 3;
+  num = num.toString().split('')
+  while (num.length > 0) {
+      arrays.push(num.splice(0, size));
+  }
+  let i = 0, j=0;
+  while(arrays.length > i) {
+    while(j < 3) {
+      if(arrays[i].length === 3) {
+        if(j === 0) {
+          toString += lowNumbers[arrays[i][j]]
+          toString += biggerNumbers[j];
+        }
+        if(j === 1) {
+          toString += tens[Number(arrays[i][j]) - 2]
+        }
+        if(j === 2) {
+          if(lowNumbers[arrays[i][j]] !== 'zero') {
+            toString += lowNumbers[Number(arrays[i][j])];
+          }
+        }
+      }
+      j++
+    }
+    j=0;
+    i++
+  }
+  return toString;
 }
 
 module.exports = numToWords;
+// console.log(numToWords(12045))
--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,26 @@
  */
 
 function permPalin(str) {
-	
+  str = str.split('');
+  let checkStr = {};
+  for(let i = 0; i < str.length; i++) {
+    if(checkStr.hasOwnProperty(str[i])) {
+      checkStr[str[i]] = true;
+    }
+    if(checkStr[str[i]] === undefined || checkStr[str[i]] !== true) {
+      checkStr[str[i]] = false;
+    }
+  }
+  let falseCount = 0;
+  for(key in checkStr) {
+    if(checkStr[key] === false) {
+      falseCount++;
+    }
+  };
+  if(falseCount >= 2) {
+    return false;
+  };
+  return true;
 }
 
 module.exports = permPalin;
