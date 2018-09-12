0674c08a1529be53a75f722f6c06fa87a6a9d6a3
Den Temple <developer@dentemple.com>
2018-09-12 17:39:40 -0400

    Solve two-sum - O(n) time

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,40 @@
  */
 
 function twoSum(arr, n) {
+  // {[n - value] : collisions}
+  const hashmap = arr.reduce((acc, value) => {
+    if (acc.hasOwnProperty(n - value)) {
+      return {
+        ...acc,
+        [n - value]: +acc[n - value] + 1
+      }
+    } else {
+      return {
+        ...acc,
+        [n - value]: 0
+      }
+    }
+  }, {})
 
+  for (let i = 0; i < arr.length; i++) {
+    if (hashmap.hasOwnProperty(arr[i])) {
+      if (n / 2 !== arr[i] || hashmap[arr[i]] > 0) {
+        return true
+      } // else continue looping
+    } // else continue looping
+  }
+
+  return false
 }
 
-module.exports = twoSum;
+// console.log(twoSum([-1, -2, -3], -6), 'should be false')
+// console.log(twoSum([1, 2, 3], 6), 'should be false')
+// console.log(twoSum([1, 1, 2, 3, 3, 4, 5], 3), 'should be true')
+// console.log(twoSum([1, 2, 3, 4, 5], 8), 'should be true')
+// console.log(twoSum([1, 2, 3, 4, 5], 0), 'should be false')
+// console.log(twoSum([1, 2, 4, 5, 6], 1), 'should be false')
+// console.log(twoSum([1, 2, 3, -3, -4], 0), 'should be true')
+// console.log(twoSum([-9, -8, -1, 1, 2], -6), 'should be true')
+// console.log(twoSum([-9, -8, -1, 1, 2], -10), 'should be true')
+
+module.exports = twoSum
