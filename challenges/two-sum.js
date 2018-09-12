71dc7a914c550906aa128bf82cd58a9cfabdb7ac
AaronAddison <abartaddison12@gmail.com>
2018-09-12 09:56:36 -0400

    twoSum complete

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,24 @@
  */
 
 function twoSum(arr, n) {
+  if (!Array.isArray(arr) || arr.length === 0 ) return false;
 
+  let obj = {};
+  let ans = false;
+
+  arr.forEach( element => {
+    obj[element] = n - element;
+  })
+
+  Object.values(obj).forEach(element => {
+    if(obj[element]){
+      ans = true;
+    }
+  })
+
+  return ans;
 }
+
+console.log(twoSum([1,2,3,4,11,9,10], 12));
 
 module.exports = twoSum;
