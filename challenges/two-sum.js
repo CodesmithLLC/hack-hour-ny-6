7703e4adaf48fd48a3653d023c8ab4ba34a52552
james-owen <james@jamesowen.io>
2018-09-13 10:04:25 -0400

    two sum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -4,6 +4,30 @@
 
 function twoSum(arr, n) {
 
+  // sort the array
+  arr.sort();
+  const midpoint = Math.floor(arr.length/2)
+
+  // cut array in half
+  const lowerHalf = arr.slice(0, midpoint);
+  const upperHalf = arr.slice(midpoint, arr.length)
+
+  // check if the largest number in lower half is larger than n
+    // if yes, divide in half again
+  if (lowerhalf[lowerHalf.length] > n) {
+    return twoSum(lowerhalf, n) 
+  }else if ()
+  }
+
+
+
+
+
+
+
+
 }
+
+twoSum([4,3,6,7,2,1], 4);
 
 module.exports = twoSum;
