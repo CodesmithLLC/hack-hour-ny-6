e88e30b2cde0e1acf54cd0e595c32d03f69512ef
Den Temple <developer@dentemple.com>
2018-09-25 09:42:51 -0400

    Complete add-linked-list

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -13,12 +13,57 @@
  */
 
 function Node(val) {
-  this.value = val;
-  this.next = null;
+  this.value = val
+  this.next = null
 }
 
 function addLinkedList(l1, l2) {
+  if (!l1 && !l2) return new Node(null)
 
+  let newHead
+
+  if (!l1) {
+    newHead = new Node(l2.value)
+  } else if (!l2) {
+    newHead = new Node(l1.value)
+  } else {
+    newHead = new Node(l1.value + l2.value)
+  }
+
+  let newNode = newHead
+  let rem = 0
+
+  while (l1 || l2 || !rem) {
+    if (!l1) {
+      newNode.next = new Node(l2.value)
+      l2 = l2.next
+    } else if (!l2) {
+      newNode.next = new Node(l1.value)
+      l1 = l1.next
+    } else if (!l1 && !l2) {
+      newNode.next = new Node(rem)
+      rem = 0
+    } else {
+      newNode.next = new Node(getSingleDigit(l1.value, l2.value))
+      rem = getTensPlace(l1.value, l2.value)
+      l1 = l1.next
+      l2 = l2.next
+    }
+  }
+
+  return newHead
+
+  //-------
+
+  function getSingleDigit(a, b) {
+    let str = a + b + ''
+    return str.slice(str.length - 1) - 0
+  }
+
+  function getTensPlace(a, b) {
+    let str = a + b + ''
+    return str.length === 2 ? str.slice(0, 1) - 0 : 0
+  }
 }
 
-module.exports = {Node: Node, addLinkedList: addLinkedList};
+module.exports = { Node: Node, addLinkedList: addLinkedList }
