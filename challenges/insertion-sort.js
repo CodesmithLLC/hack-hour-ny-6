ad0692ec82167d2031ab56d4be4cfa8507d05401
AaronAddison <abartaddison12@gmail.com>
2018-10-09 09:44:49 -0400

    insertion-sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -4,8 +4,25 @@
 
 
 
-function insertionSort(array) {
+function insertionSort(array, indx=1) {
+ if( indx === array.length){
+   return array;
+ }
 
+ if( array[indx] < array[indx - 1] ){
+  let lower = array[indx] ;
+  let higher = array[indx - 1]
+  array[indx - 1 ] = lower;
+  array[indx] = higher
+  return insertionSort(array, indx - 1, indx);
+ }
+ 
+ return insertionSort(array, indx + 1);
 }
+
+let arr = [2,6,9,1,4,5,3,7,8,0];
+
+insertionSort(arr);
+
 
 module.exports = insertionSort;
